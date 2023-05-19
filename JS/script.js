f5a9b9aeaa69d91fc07920b2dc0c5ce9b6853436
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-05-18 22:47:54 -0300

    adicionando o JS

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+document.getElementById('titulo')
+
+document.getElementsByClassName('classezinha')
+
+document.getElementsByTagName('div')
+
+document.querySelector('.classezinha')
+
+document.querySelectorAll('.classezinha')
